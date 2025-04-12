c8e79cbffaaddb9b836e796e0e82ac19a0a590f8
acerDebugman <460873250@qq.com>
2025-04-13 07:43:16 +0800

    update

--- exercises/traits/traits5.rs
+++ exercises/traits/traits5.rs
@@ -7,8 +7,6 @@
 // Execute `rustlings hint traits5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub trait SomeTrait {
     fn some_function(&self) -> bool {
         true
@@ -29,12 +27,27 @@ impl OtherTrait for SomeStruct {}
 impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
+// auto trait 才可以组合配合 &dyn xxx 使用
+// auto trait 是没有方法的trait, 这样才可以保证编译器预先知道大小，进行内存布局；
+// 普通的trait进行组合，可能无法让编译器准确的进行内存布局
+trait ParentTrait: SomeTrait + OtherTrait {}
+impl<T: SomeTrait + OtherTrait> ParentTrait for T {}
+
 // YOU MAY ONLY CHANGE THE NEXT LINE
-fn some_func(item: ??) -> bool {
+// 这个只能 auto trait 才可以进行 +， 普通 trait 不能相加
+// fn some_func<T: SomeTrait + OtherTrait>(item: impl T) -> bool {
+fn some_func(item: &dyn ParentTrait) -> bool {
     item.some_function() && item.other_function()
 }
 
+// 这个肯定可以的
+// fn some_func(item: impl SomeTrait + OtherTrait) -> bool {
+//     item.some_function() && item.other_function()
+// }
+
 fn main() {
-    some_func(SomeStruct {});
-    some_func(OtherStruct {});
+    // some_func(SomeStruct {});
+    // some_func(OtherStruct {});
+    some_func(&SomeStruct {});
+    some_func(&OtherStruct {});
 }
